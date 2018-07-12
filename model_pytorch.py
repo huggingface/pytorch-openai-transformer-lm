5f3348430dd9e34b4ef991d7fe4fd5f015f4bf6f
Grégory Châtel <chatel.gregory@gmail.com>
2018-07-12 14:34:22 +0200

    New ClfHead module.

--- model_pytorch.py
+++ model_pytorch.py
@@ -214,6 +214,26 @@ class MultipleChoiceHead(nn.Module):
         clf_logits = self.linear(clf_h)
         return clf_logits.view(-1, x.size(1))
 
+class ClfHead(nn.Module):
+    """ TODO: test this class."""
+    def __init__(self, clf_token, cfg, n_class):
+        super(ClfHead, self).__init__()
+        self.n_embd = clf.n_embd
+        self.clf_token = clf_token
+        self.dropout = nn.Dropout(cfg.clf_pdrop)
+        self.linear = nn.Linear(cfg.n_embd, n_class)
+
+        nn.init.normal_(self.linear.weight, std=0.02)
+        nn.init.normal_(self.linear.bias, 0)
+
+    def forward(self, h, x):
+        clf_h = h.view(-1, self.n_embd)
+        flat = x[..., 0].contiguous().view(-1)
+        clf_h = clf_h[flat == self.clf_token, :]
+        clf_logits = self.linear(clf_h)
+
+        return clf_logits
+
 
 class DoubleHeadModel(nn.Module):
     """ Transformer with language model and multiple choice heads """
